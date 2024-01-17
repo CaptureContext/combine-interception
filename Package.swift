84a04ec7f695d2e053fbd242dfe08dc9fdd7d3f5
maximkrouk <id.maximkrouk@gmail.com>
2024-01-17 04:59:02 +0100

    cleanup: Use tabs for indentation

--- Package.swift
+++ Package.swift
@@ -3,27 +3,35 @@
 import PackageDescription
 
 let package = Package(
-  name: "combine-interception",
-  platforms: [
-    .iOS(.v13),
-    .macOS(.v10_15),
-    .tvOS(.v13),
-    .watchOS(.v6)
-  ],
-  products: [
-    .library(
-      name: "CombineInterception",
-      type: .static,
-      targets: ["CombineInterception"]
-    )
-  ],
-  targets: [
-    .target(
-      name: "CombineInterception",
-      dependencies: [
-        .target(name: "CombineRuntime")
-      ]
-    ),
-    .target(name: "CombineRuntime")
-  ]
+	name: "combine-interception",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.macCatalyst(.v13),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(
+			name: "CombineInterception",
+			type: .static,
+			targets: ["CombineInterception"]
+		)
+	],
+	targets: [
+		.target(
+			name: "CombineInterception",
+			dependencies: [
+				.target(name: "CombineRuntime")
+			]
+		),
+		.target(name: "CombineRuntime"),
+
+		.testTarget(
+			name: "CombineInterceptionTests",
+			dependencies: [
+				.target(name: "CombineInterception")
+			]
+		)
+	]
 )
--- Sources/CombineInterception/ObjC+Runtime.swift
+++ Sources/CombineInterception/ObjC+Runtime.swift
@@ -9,19 +9,19 @@ import Foundation
 ///
 /// - returns: The matching method, or `nil` if none is found.
 internal func class_getImmediateMethod(_ `class`: AnyClass, _ selector: Selector) -> Method? {
-  var total: UInt32 = 0
-
-  if let methods = class_copyMethodList(`class`, &total) {
-    defer { free(methods) }
-
-    for index in 0..<Int(total) {
-      let method = methods[index]
-
-      if method_getName(method) == selector {
-        return method
-      }
-    }
-  }
-
-  return nil
+	var total: UInt32 = 0
+	
+	if let methods = class_copyMethodList(`class`, &total) {
+		defer { free(methods) }
+		
+		for index in 0..<Int(total) {
+			let method = methods[index]
+			
+			if method_getName(method) == selector {
+				return method
+			}
+		}
+	}
+	
+	return nil
 }
